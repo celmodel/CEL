6ffc4665c88ed923faa51bf429ada0345aeaeff0
celmodel <49870022+celmodel@users.noreply.github.com>
2019-05-30 21:20:21 +0300

    Add files via upload

---
+++ CELmodel_LS2019.py
@@ -0,0 +1,36 @@
+''' 3D visualization of Locked Shields 2019 exercise CEL model data in Jupiter Notebook
+    Author: Ragnar Kobin
+'''
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+%matplotlib notebook
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+_x = np.arange(9)
+_y = np.arange(5)
+_xx, _yy = np.meshgrid(_x, _y)
+x, y = _xx.ravel(), _yy.ravel()
+dz = x + y
+z = np.zeros_like(dz)
+dx = dy = 0.5
+
+#LS19
+dz=  [1,1,1,1,1,0,0,0,0,   0,1,1,0,1,0,0,0,0,  0,0,0,0,0,0,1,0,0,  1,1,0,0,0,1,0,0,1,  0,3.1,0.9,3.4,1.6,0,1,0,0]
+
+ax.bar3d(x, y, z, dx, dy, dz, color='b')
+
+ax.set_title('Locked Shields 2019 CEL model')
+ax.set_xlabel('Level')
+ax.set_ylabel('')
+ax.set_zlabel('Occurrence')
+
+locs, labels = plt.yticks()
+plt.yticks(np.arange(6), ('', 'Level', 'Sector', 'Type', 'CI vector', 'Category'), rotation=5)
+
+plt.gca().invert_xaxis()
+plt.show()
